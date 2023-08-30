c27de826c19853d9dcaf28fcc740aa3a2f60f16e
ducctrong <trongld21@gmail.com>
2023-08-30 20:38:49 +0700

    add facebook chat page

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,16 @@ import { firestore } from "../firebase";
 import { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import Link from "next/link";
+import Facebook from "../components/Facebook";
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center flex-col w-screen h-screen">
       <h1 className="text-3xl font-semibold">Tiệm Cà Phê Ông Quan</h1>
       <h2 className="italic">Coming Soon</h2>
+      <div className="fixed rounded-full bottom-8 right-8 z-[60]">
+        <Facebook />
+      </div>
     </div>
   );
 }
