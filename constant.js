694e4779f5fd4f9a65a3ed3b46cef5bd87a324c9
Khang Tran <Khangtt1110@gmail.com>
2023-09-01 15:34:59 +0700

    Add filter by date

--- constant.js
+++ constant.js
@@ -11,10 +11,42 @@ export const formatDateTime = (timestamp) => {
     year: "numeric",
     month: "numeric",
     day: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: false, // Sử dụng định dạng 24 giờ
+    // hour: "2-digit",
+    // minute: "2-digit",
+    // second: "2-digit",
+    // hour12: false, // Sử dụng định dạng 24 giờ
   };
   return new Intl.DateTimeFormat("vi-VN", options).format(new Date(timestamp));
 };
+
+export const columnsThuChi = ({handleClickDetail}) => [
+  {
+    title: "Ngày",
+    dataIndex: "date",
+    render: text => <p onClick={() => handleClickDetail()}>{text}</p>
+  },
+  {
+    title: "Ca 1",
+    dataIndex: "ca1",
+  },
+  {
+    title: "Ca 2",
+    dataIndex: "ca2",
+  },
+  {
+    title: "Ca 3",
+    dataIndex: "ca3",
+  },
+  {
+    title: "Tổng danh thu",
+    dataIndex: "totalRevenue",
+  },
+  {
+    title: "Tổng chi",
+    dataIndex: "totalExpenditure",
+  },
+  {
+    title: "Thực nhận",
+    dataIndex: "actuallyReceived",
+  },
+];
