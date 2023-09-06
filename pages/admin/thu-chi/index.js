f9831606ee4c5421f0a387fafe928544b3e42109
ducctrong <trongld21@gmail.com>
2023-09-06 20:33:32 +0700

    chaneg layout

--- pages/admin/thu-chi/index.js
+++ pages/admin/thu-chi/index.js
@@ -1,16 +1,76 @@
-import React, { useState, useEffect } from "react";
+import { Button, Modal, Select, Table } from "antd";
+import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { firestore } from "../../../firebase";
-import Link from "next/link";
-import { format } from "date-fns";
-import ChiTietCa from "../../../components/ChiTietCa";
 import { formatDateTime, formatVND } from "../../../constant";
+import AdminLayout from "../../../components/Layout/AdminLayout";
+import Link from "next/link";
+import { DatePicker } from "antd";
+const { RangePicker } = DatePicker;
+import { Typography } from "antd";
+const { Text } = Typography;
 
-export default function QuanLyThuChi() {
+function ThuChi() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [transactions, setTransactions] = useState([]);
-  const [selectedShift, setSelectedShift] = useState("Ca 1");
-  const [selectedDate, setSelectedDate] = useState(null);
+  const [dataTable, setDataTable] = useState();
+  const [transactionsDetail, setTransactionsDetail] = useState();
+  const [dateRange, setDateRange] = useState();
 
+  let transactionsTotal = [];
+  // Function to calculator each element in array
+  transactions.forEach((item) => {
+    const timestamp = item.timestamp;
+    if (!transactionsTotal[timestamp]) {
+      transactionsTotal[timestamp] = {
+        totalRevenue: 0,
+        totalExpenditure: 0,
+        entries: [],
+      };
+    }
+    transactionsTotal[timestamp].totalRevenue += item.revenue || 0;
+    transactionsTotal[timestamp].totalExpenditure += item.expenditure || 0;
+    transactionsTotal[timestamp].entries.push(item);
+  });
+
+  // Format list data
+  const resultArray = Object.keys(transactionsTotal).map((timestamp) => ({
+    timestamp,
+    totalRevenue: transactionsTotal[timestamp].totalRevenue,
+    totalExpenditure: transactionsTotal[timestamp].totalExpenditure,
+    entries: transactionsTotal[timestamp].entries.sort((a, b) => {
+      const shiftOrder = { "Ca 1": 1, "Ca 2": 2, "Ca 3": 3 };
+      return shiftOrder[a.shift] - shiftOrder[b.shift];
+    }),
+  }));
+
+  const dataResult = resultArray.map((item) => ({
+    key: item.timestamp,
+    date: item.timestamp || "",
+    ca1: item.entries[0],
+    ca2: item.entries[1],
+    ca3: item.entries[2],
+    totalRevenue: item.totalRevenue,
+    totalExpenditure: item.totalExpenditure,
+    actuallyReceived: item.totalRevenue - item.totalExpenditure,
+  }));
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleDateChange = (date, dateString) => {
+    if (date === null) {
+      setDateRange(null);
+    } else {
+      setDateRange(dateString);
+    }
+  };
+
+  // Fetch data from firebase
   useEffect(() => {
     const fetchTransactions = async () => {
       try {
@@ -18,152 +78,188 @@ export default function QuanLyThuChi() {
           collection(firestore, "transactions")
         );
         const transactionList = querySnapshot.docs.map((doc) => doc.data());
+        transactionList.sort((a, b) => {
+          return new Date(b.timestamp) - new Date(a.timestamp);
+        });
         setTransactions(transactionList);
       } catch (error) {
         console.error("Lỗi khi tải danh sách giao dịch:", error);
       }
     };
 
+    // Fetch data
     fetchTransactions();
   }, []);
 
-  const shifts = ["Ca 1", "Ca 2", "Ca 3"];
-
-  const handleShiftClick = (date, shift) => {
-    setSelectedDate(date);
-    setSelectedShift(shift);
+  const handleClickDetail = (text) => {
+    setTransactionsDetail(text);
+    setIsModalOpen(true);
   };
 
-  // Định nghĩa đối tượng lưu trữ thông tin doanh thu của từng ca
-  const shiftRevenues = {};
+  const columns = [
+    {
+      title: "Ngày",
+      dataIndex: "date",
+      render: (text) => <p>{formatDateTime(new Date(text), "dd/MM/yyyy")}</p>,
+    },
+    {
+      title: "Ca 1",
+      dataIndex: "ca1",
+      render: (text) => (
+        <p
+          className="cursor-pointer underline"
+          onClick={() => handleClickDetail(text)}
+        >
+          {formatVND(text.revenue || 0)}
+        </p>
+      ),
+    },
+    {
+      title: "Ca 2",
+      dataIndex: "ca2",
+      render: (text) => (
+        <p
+          className="cursor-pointer underline"
+          onClick={() => handleClickDetail(text)}
+        >
+          {formatVND(text?.revenue || 0)}
+        </p>
+      ),
+    },
+    {
+      title: "Ca 3",
+      dataIndex: "ca3",
+      render: (text) => (
+        <p
+          className="cursor-pointer underline"
+          onClick={() => handleClickDetail(text)}
+        >
+          {formatVND(text?.revenue || 0)}
+        </p>
+      ),
+    },
+    {
+      title: "Tổng danh thu",
+      dataIndex: "totalRevenue",
+      render: (text) => <p>{formatVND(text || 0)}</p>,
+    },
+    {
+      title: "Tổng chi",
+      dataIndex: "totalExpenditure",
+      render: (text) => <p>{formatVND(text || 0)}</p>,
+    },
+    {
+      title: "Thực nhận",
+      dataIndex: "actuallyReceived",
+      render: (text) => <p>{formatVND(text || 0)}</p>,
+    },
+  ];
 
-  // Lặp qua các giao dịch để tính tổng doanh thu của từng ca
-  transactions.forEach((transaction) => {
-    const timestamp = new Date(transaction.timestamp)
-      .toISOString()
-      .split("T")[0];
-    if (!shiftRevenues[timestamp]) {
-      shiftRevenues[timestamp] = {};
+  useEffect(() => {
+    if (transactions && dateRange) {
+      setDataTable(
+        dataResult.filter((item) => {
+          const itemDate = new Date(item?.date);
+          return (
+            itemDate >= new Date(dateRange[0]) &&
+            itemDate <= new Date(dateRange[1])
+          );
+        })
+      );
     }
-    if (!shiftRevenues[timestamp][transaction.shift]) {
-      shiftRevenues[timestamp][transaction.shift] = 0;
-    }
-    shiftRevenues[timestamp][transaction.shift] += transaction.revenue;
-  });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dateRange]);
 
-  // Định nghĩa đối tượng lưu trữ thông tin tổng doanh thu, tổng chi và thực nhận theo ngày
-  const dailyInfo = {};
-
-  // Lặp qua các giao dịch để tính tổng doanh thu, tổng chi và thực nhận theo ngày
-  transactions.forEach((transaction) => {
-    const timestamp = new Date(transaction.timestamp)
-      .toISOString()
-      .split("T")[0];
-    if (!dailyInfo[timestamp]) {
-      dailyInfo[timestamp] = {
-        totalRevenue: 0,
-        totalExpenditure: 0,
-      };
-    }
-    dailyInfo[timestamp].totalRevenue += transaction.revenue;
-    dailyInfo[timestamp].totalExpenditure += transaction.expenditure;
-  });
+  useEffect(() => {}, [transactions]);
 
   return (
-    <div className="p-16">
-      <h1 className="text-xl font-semibold mb-4">Quản lý Thu Chi</h1>
-      <div className="mb-4 flex justify-start gap-4 flex-wrap">
-        <Link href="/admin" className="p-2 bg-green-400 rounded-md text-white">
-          Về trang chủ của admin
-        </Link>
-
-        <Link
-          href="/admin/thu-chi/nhap-thu-chi"
-          className="p-2 bg-blue-400 rounded-md text-white"
-        >
-          Nhập thu chi
-        </Link>
-      </div>
-      <table className="min-w-full">
-        <thead>
-          <tr className="bg-gray-200">
-            <th className="py-2 px-4 text-center">Ngày</th>
-            {shifts.map((shift) => (
-              <th
-                key={shift}
-                className="py-2 px-4 cursor-pointer"
-                onClick={() => handleShiftClick(selectedDate, shift)}
-              >
-                {shift}
-              </th>
-            ))}
-            <th>Tổng Doanh Thu</th>
-            <th>Tổng Chi</th>
-            <th>Thực nhận</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* {Object.keys(shiftRevenues).map((timestamp) => (
-            <tr key={timestamp} className="border-b">
-              <td className="py-2 px-4 text-center">
-                {formatDateTime(new Date(timestamp), "dd/MM/yyyy")}
-              </td>
-              {shifts.map((shift) => (
-                <td
-                  key={shift}
-                  className="py-2 px-4 cursor-pointer text-center"
-                  onClick={() => handleShiftClick(timestamp, shift)}
-                >
-                  {formatVND(shiftRevenues[timestamp][shift] || 0)}
-                </td>
-              ))}
-             
-            </tr>
-          ))} */}
-
-          {Object.keys(dailyInfo).map((timestamp) => (
-            <tr key={timestamp} className="border-b">
-              <td className="py-2 px-4 text-center">
-                {formatDateTime(new Date(timestamp), "dd/MM/yyyy")}
-              </td>
-              {shifts.map((shift) => (
-                <td
-                  key={shift}
-                  className="py-2 px-4 cursor-pointer text-center"
-                  onClick={() => handleShiftClick(timestamp, shift)}
-                >
-                  {formatVND(shiftRevenues[timestamp]?.[shift] || 0)}
-                </td>
-              ))}
-              <td className="py-2 px-4 text-center">{formatVND(dailyInfo[timestamp]?.totalRevenue || 0)}</td>
-              <td className="py-2 px-4 text-center">{formatVND(dailyInfo[timestamp]?.totalExpenditure || 0)}</td>
-              <td className="py-2 px-4 text-center">
-                {formatVND(
-                  (dailyInfo[timestamp]?.totalRevenue || 0) -
-                    (dailyInfo[timestamp]?.totalExpenditure || 0)
-                )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      {selectedDate && (
-        <div className="bg-[rgba(0,0,0,0.75)] p-16 w-screen h-screen fixed top-0 left-0 right-0 bottom-0 ">
-          <ChiTietCa
-            transactions={transactions}
-            selectedDate={selectedDate}
-            selectedShift={selectedShift}
-          />
-          <button
-            className="absolute top-8 right-16 text-white"
-            onClick={() => {
-              setSelectedDate();
-            }}
-          >
-            X
-          </button>
+    <AdminLayout>
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-between">
+          <div className="flex gap-4">
+            <Link
+              href="/admin"
+              className="p-2 bg-green-400 rounded-md text-white"
+            >
+              Về trang chủ của admin
+            </Link>
+            <Link
+              href="/admin/thu-chi/nhap-thu-chi"
+              className="p-2 bg-blue-400 rounded-md text-white"
+            >
+              Nhập thu chi
+            </Link>
+          </div>
+          <RangePicker onChange={handleDateChange} />
         </div>
-      )}
-    </div>
+        <Table
+          columns={columns}
+          dataSource={!!dateRange ? dataTable : dataResult}
+          summary={(pageData) => {
+            let revenue = 0;
+            let expenditure = 0;
+            let received = 0;
+            pageData.forEach(({ totalRevenue, totalExpenditure, actuallyReceived }) => {
+              revenue += totalRevenue;
+              expenditure += totalExpenditure;
+              received += actuallyReceived;
+            });
+            return (
+              <>
+                <Table.Summary.Row>
+                  {/* Display empty cells for the first two columns */}
+                  <Table.Summary.Cell index={0}>Tổng</Table.Summary.Cell>
+                  <Table.Summary.Cell index={1}></Table.Summary.Cell>
+                  <Table.Summary.Cell index={2}></Table.Summary.Cell>
+                  <Table.Summary.Cell index={3}></Table.Summary.Cell>
+
+                  <Table.Summary.Cell index={4}>
+                    <Text type="" className="font-bold">{formatVND(revenue || 0)}</Text>
+                  </Table.Summary.Cell>
+
+                  <Table.Summary.Cell index={5}>
+                  <Text type="danger" className="font-bold">{formatVND(expenditure || 0)}</Text>
+                  </Table.Summary.Cell>
+                  <Table.Summary.Cell index={6}>
+                  <Text type="success" className="font-bold">{formatVND(received || 0)}</Text>
+                  </Table.Summary.Cell>
+                </Table.Summary.Row>
+              </>
+            );
+          }}
+        />
+      </div>
+      <Modal
+        title="Chi Tiết Thu Chi"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>
+          Ca trực: <b>{transactionsDetail?.shift}</b>
+        </p>
+        <p>
+          Thời gian: <b>{transactionsDetail?.timestamp}</b>
+        </p>
+        <p>
+          Tiền mặt: <b>{formatVND(transactionsDetail?.cashAmount)}</b>
+        </p>
+        <p>
+          Tiền chuyển khoản:{" "}
+          <b>{formatVND(transactionsDetail?.transferAmount || 0)}</b>
+        </p>
+        <p>
+          Doanh thu: <b>{formatVND(transactionsDetail?.revenue || 0)}</b>
+        </p>
+        <p>
+          Tổng chi: <b>{formatVND(transactionsDetail?.expenditure || 0)}</b>
+        </p>
+        {!!transactionsDetail?.expenditure && (
+          <p>Nội dung chi: {transactionsDetail?.expenditureContent}</p>
+        )}
+      </Modal>
+    </AdminLayout>
   );
 }
+
+export default ThuChi;
